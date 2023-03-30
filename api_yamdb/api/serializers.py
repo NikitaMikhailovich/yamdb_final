2442d1199afeed732e30e0dba7abedafdb20f540
Никита Гайнов <nikita.gaynov@mail.ru>
2023-03-30 22:32:43 +0300

    first commit

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -4,7 +4,6 @@ import re
 
 from django.shortcuts import get_object_or_404
 from rest_framework import serializers
-
 from reviews.models import Category, Comment, Genre, Review, Title
 from users.models import User
 
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -13,7 +13,6 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
-
 from api_yamdb.settings import SENDER
 from reviews.models import Category, Genre, Review, Title
 from users.models import User
