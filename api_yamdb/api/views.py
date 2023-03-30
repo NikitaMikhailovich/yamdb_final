3b05ae87b17f48f363d8fce556c68f0aeea6154c
Никита Гайнов <nikita.gaynov@mail.ru>
2023-03-30 22:38:20 +0300

    first commit

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -15,8 +15,8 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import AccessToken
 from api_yamdb.settings import SENDER
 from reviews.models import Category, Genre, Review, Title
-from users.models import User
 
+from users.models import User
 from .filters import TitleFilter
 from .mixins import CreateListDestroyViewSet
 from .permissons import (AdminOnly, AuthorModeratorAdminOrReadOnly,
